798e28cfb6233eadae4331c1e12e4cc334ef16dc
IFFANCOE <vbaako1@hotmail.com>
2020-03-19 14:42:55 +0700

    firebase

---
+++ src/config.js
@@ -0,0 +1,13 @@
+
+const config = {
+    apiKey: "AIzaSyDG99M-uQ5DwrgMQtFv3j-oF7Zcx9s1_Rs",
+    authDomain: "todo240311.firebaseapp.com",
+    databaseURL: "https://todo240311.firebaseio.com",
+    projectId: "todo240311",
+    storageBucket: "todo240311.appspot.com",
+    messagingSenderId: "808134885686",
+    appId: "1:808134885686:web:2cef9d66f83efcb296f397",
+    measurementId: "G-SE8V9FR2JV"
+  };
+
+  export default config;
